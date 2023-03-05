c520d90062d03409971b407a8c2be89e2b8d8448
Konrad <konrad.wieckiewicz@gmail.com>
2023-03-05 16:59:43 +0100

    Added 2 first missing scripts
    Resolved task accordingly to given instructions

--- Lab-1/main-5.py
+++ Lab-1/main-5.py
@@ -4,13 +4,32 @@ import numpy as np
 startX = int(input('Provide starting x: '))
 endX = int(input('Provide ending x: '))
 
-x = np.linspace(startX, endX, 60) # Changed num from 10 to 60 to smooth sin(x)
-
+x = np.linspace(startX, endX, 200)           # Changed num from 10 to 60 to smooth sin(x), Changed form 60 to 200 (depends on the x size)
 _, ax = plt.subplots(figsize=(10, 5))
-ax.plot(x, x, color='black', label='y=x')
-ax.plot(x, np.sin(x), label='y=sin(x)')
-ax.plot(x, np.cos(x), label='y=cox(x)')
-ax.plot(x, np.power(x, 3), label='y=x^3')
+
+functions = {
+                 'x':  (x, 'y=x'),
+                 'x2': (x**2, 'y=x^2'),
+                 'x3': (np.power(x, 3), 'y=x^3'),
+                 'sin': (np.sin(x), 'y=sin(x)'),
+                 'cos': (np.cos(x), 'y=cos(x)'),
+             }
+print('Pick functions to display, write them all using separetor' +
+      '\nx -> y=x' +
+      '\nx2 -> y=x^2' +
+      '\nx3 -> y=x^3' +
+      '\nsin -> y=sin(x)' +
+      '\ncos -> y=cos(x)' +
+      'example: x,x2,x3')
+print()
+
+picked_functions = str(input('Provide functions by delimeter')).split(',')
+
+for fun in picked_functions:
+    chart_color = str(input(f'Provide chart color for {fun} function'))
+    function, label = functions[fun]
+    ax.plot(x, function, color=chart_color, label=label)
+
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_title("Wykres")
